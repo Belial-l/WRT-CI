8804cfc7e5d0293a2dfd504bf691aff8edeaba89
Belial-l <cx1447@outlook.com>
2026-09-20 04:38:02 +0800

    Update generate_config function to clean config file
    Remove CONFIG_OVERRIDE_PKGS line from generated config file.

--- Scripts/function.sh
+++ Scripts/function.sh
@@ -143,6 +143,9 @@ function generate_config() {
   config_file=".config"
   #如配置文件已存在
   cat $GITHUB_WORKSPACE/Config/${WRT_CONFIG}.txt $GITHUB_WORKSPACE/Config/GENERAL.txt  > $config_file
+  
+  sed -i '/^CONFIG_OVERRIDE_PKGS=/d' "$config_file"
+  
   local target=$(echo $WRT_ARCH | cut -d'_' -f2)
 
   #删除wifi依赖
