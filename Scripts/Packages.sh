274923c9627fd96eb641cca44fd0999b701b8bee
Belial-l <cx1447@outlook.com>
2026-09-22 01:25:04 +0800

    Remove commented-out luci-app-dae package update

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -76,8 +76,8 @@ UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "axonhub gecoosac luci-ap
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 # UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "kix"
+# UPDATE_PACKAGE "luci-app-dae" "davidtall/luci-app-dae" "kix"
 UPDATE_PACKAGE "daede" "kenzok8/openwrt-daede" "main" "pkg"
-UPDATE_PACKAGE "luci-app-dae" "davidtall/luci-app-dae" "kix"
 UPDATE_PACKAGE "luci-app-pushbot" "zzsj0928/luci-app-pushbot" "master"
 UPDATE_PACKAGE "luci-app-nginx-manager" "hello-yunshu/luci-app-nginx-manager" "main"
 
